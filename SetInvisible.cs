eb30252cdd9a8f686f3b6387c17645e5e42e6ced
Simon Schachinger <simon.schachinger@elev.ga.lbs.se>
2019-01-17 10:44:50 +0100

    finalCommit2

---
+++ SetInvisible.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetInvisible : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {//gör att ett objekt inte syns i spelet. brukar användas till t.ex skyboxes
+        GetComponent<SpriteRenderer>().enabled = false;
+    }
+
+
+}
